958a674dc45c3c4df898ddadb392d8e7c8afd765
Kapil Maharjan <104562032+kapil-maharjan@users.noreply.github.com>
2026-06-09 20:55:53 +0700

    Refactor quality checks SQL script for consistency
    Updated comments and improved formatting for clarity.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,264 +1,159 @@
 /*
-===============================================
+===============================================================================
 Quality Checks
-===============================================
+===============================================================================
 Script Purpose:
-  This script performs various quality checks for data consistency, accuracy,
-  and standardization across the 'silver' schema. It includes checks for:
-  - Null or duplicate primary keys.
-  - Unwanted spaces in string field.
-  - Data Standardization and consistency.
-  - Invalid date ranges and orders.
-  - Data consistency between related fields.
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
 
 Usage Notes:
-  - Run these checks after data loading Silver Layer.
-  - Investigate and resolve any discrepancies found during the checks.
-=========================================================================
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE silver.load_silver AS
-BEGIN
-    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
-    
-	BEGIN TRY
-        SET @batch_start_time = GETDATE();
-        PRINT '================================================';
-        PRINT 'Loading Silver Layer';
-        PRINT '================================================';
+-- ====================================================================
+-- Checking 'silver.crm_cust_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+    cst_id,
+    COUNT(*) 
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
-		PRINT '------------------------------------------------';
-		PRINT 'Loading CRM Tables';
-		PRINT '------------------------------------------------';
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    cst_key 
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
 
-		-- Loading silver.crm_cust_info
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_cust_info';
-		TRUNCATE TABLE silver.crm_cust_info;
-		PRINT '>> Inserting Data Into: silver.crm_cust_info';
-		
-		INSERT INTO silver.crm_cust_info (
-			cst_id, 
-			cst_key, 
-			cst_firstname, 
-			cst_lastname, 
-			cst_marital_status, 
-			cst_gndr,
-			cst_create_date
-		)
-		SELECT
-			cst_id,
-			cst_key,
-			TRIM(cst_firstname) AS cst_firstname,
-			TRIM(cst_lastname) AS cst_lastname,
-			CASE 
-				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-				ELSE 'n/a'
-			END AS cst_marital_status,
-			CASE 
-				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-				ELSE 'n/a'
-			END AS cst_gndr,
-			cst_create_date
-		FROM (
-			SELECT
-				*,
-				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-			FROM bronze.crm_cust_info
-			WHERE cst_id IS NOT NULL
-		) t
-		WHERE flag_last = 1;
-		
-		SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    cst_marital_status 
+FROM silver.crm_cust_info;
 
-		-- Loading silver.crm_prd_info
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_prd_info';
-		TRUNCATE TABLE silver.crm_prd_info;
-		PRINT '>> Inserting Data Into: silver.crm_prd_info';
-		
-		INSERT INTO silver.crm_prd_info (
-			prd_id,
-			cat_id,
-			prd_key,
-			prd_nm,
-			prd_cost,
-			prd_line,
-			prd_start_dt,
-			prd_end_dt
-		)
-		SELECT
-			prd_id,
-			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
-			prd_nm,
-			ISNULL(prd_cost, 0) AS prd_cost,
-			CASE 
-				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-				ELSE 'n/a'
-			END AS prd_line,
-			CAST(prd_start_dt AS DATE) AS prd_start_dt,
-			CAST(
-				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
-				AS DATE
-			) AS prd_end_dt
-		FROM bronze.crm_prd_info;
-		
-        SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+-- ====================================================================
+-- Checking 'silver.crm_prd_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+    prd_id,
+    COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
-        -- Loading crm_sales_details
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_sales_details';
-		TRUNCATE TABLE silver.crm_sales_details;
-		PRINT '>> Inserting Data Into: silver.crm_sales_details';
-		
-		INSERT INTO silver.crm_sales_details (
-			sls_ord_num,
-			sls_prd_key,
-			sls_cust_id,
-			sls_order_dt,
-			sls_ship_dt,
-			sls_due_dt,
-			sls_sales,
-			sls_quantity,
-			sls_price
-		)
-		SELECT 
-			sls_ord_num,
-			sls_prd_key,
-			sls_cust_id,
-			CASE 
-				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
-				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-			END AS sls_order_dt,
-			CASE 
-				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
-				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-			END AS sls_ship_dt,
-			CASE 
-				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
-				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-			END AS sls_due_dt,
-			CASE 
-				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
-					THEN sls_quantity * ABS(sls_price)
-				ELSE sls_sales
-			END AS sls_sales,
-			sls_quantity,
-			CASE 
-				WHEN sls_price IS NULL OR sls_price <= 0 
-					THEN sls_sales / NULLIF(sls_quantity, 0)
-				ELSE sls_price
-			END AS sls_price
-		FROM bronze.crm_sales_details;
-		
-        SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
 
-        -- Loading erp_cust_az12
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_cust_az12';
-		TRUNCATE TABLE silver.erp_cust_az12;
-		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
-		
-		INSERT INTO silver.erp_cust_az12 (
-			cid,
-			bdate,
-			gen
-		)
-		SELECT
-			CASE
-				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
-				ELSE cid
-			END AS cid, 
-			CASE
-				WHEN bdate > GETDATE() THEN NULL
-				ELSE bdate
-			END AS bdate,
-			CASE
-				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-				ELSE 'n/a'
-			END AS gen
-		FROM bronze.erp_cust_az12;
-		
-	    SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
 
-		PRINT '------------------------------------------------';
-		PRINT 'Loading ERP Tables';
-		PRINT '------------------------------------------------';
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
 
-        -- Loading erp_loc_a101
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_loc_a101';
-		TRUNCATE TABLE silver.erp_loc_a101;
-		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
-		
-		INSERT INTO silver.erp_loc_a101 (
-			cid,
-			cntry
-		)
-		SELECT
-			REPLACE(cid, '-', '') AS cid, 
-			CASE
-				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
-				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-				ELSE TRIM(cntry)
-			END AS cntry
-		FROM bronze.erp_loc_a101;
-		
-	    SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
-		
-		-- Loading erp_px_cat_g1v2
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-		TRUNCATE TABLE silver.erp_px_cat_g1v2;
-		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-		
-		INSERT INTO silver.erp_px_cat_g1v2 (
-			id,
-			cat,
-			subcat,
-			maintenance
-		)
-		SELECT
-			id,
-			cat,
-			subcat,
-			maintenance
-		FROM bronze.erp_px_cat_g1v2;
-		
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+-- Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
 
-		SET @batch_end_time = GETDATE();
-		PRINT '==========================================';
-		PRINT 'Loading Silver Layer is Completed';
-        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-		PRINT '==========================================';
-	END TRY
-	BEGIN CATCH
-		PRINT '==========================================';
-		PRINT 'ERROR OCCURRED DURING SILVER LAYER LOADING';
-		PRINT 'Error Message: ' + ERROR_MESSAGE();
-		PRINT 'Error Severity: ' + CAST(ERROR_SEVERITY() AS NVARCHAR);
-		PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR);
-		PRINT '==========================================';
-	END CATCH
-END;
+-- ====================================================================
+-- Checking 'silver.crm_sales_details'
+-- ====================================================================
+-- Check for Invalid Dates
+-- Expectation: No Invalid Dates
+SELECT 
+    NULLIF(sls_due_dt, 0) AS sls_due_dt 
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0 
+    OR LEN(sls_due_dt) != 8 
+    OR sls_due_dt > 20500101 
+    OR sls_due_dt < 19000101;
+
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt 
+   OR sls_order_dt > sls_due_dt;
+
+-- Check Data Consistency: Sales = Quantity * Price
+-- Expectation: No Results
+SELECT DISTINCT 
+    sls_sales,
+    sls_quantity,
+    sls_price 
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+   OR sls_sales IS NULL 
+   OR sls_quantity IS NULL 
+   OR sls_price IS NULL
+   OR sls_sales <= 0 
+   OR sls_quantity <= 0 
+   OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+-- ====================================================================
+-- Checking 'silver.erp_cust_az12'
+-- ====================================================================
+-- Identify Out-of-Range Dates
+-- Expectation: Birthdates between 1924-01-01 and Today
+SELECT DISTINCT 
+    bdate 
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' 
+   OR bdate > GETDATE();
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    gen 
+FROM silver.erp_cust_az12;
+
+-- ====================================================================
+-- Checking 'silver.erp_loc_a101'
+-- ====================================================================
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    cntry 
+FROM silver.erp_loc_a101
+ORDER BY cntry;
+
+-- ====================================================================
+-- Checking 'silver.erp_px_cat_g1v2'
+-- ====================================================================
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) 
+   OR subcat != TRIM(subcat) 
+   OR maintenance != TRIM(maintenance);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    maintenance 
+FROM silver.erp_px_cat_g1v2;
